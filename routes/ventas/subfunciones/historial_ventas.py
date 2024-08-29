196b73d60522cc0e58f8c15e37a64c9542370ae8
adrian <adrianescalona228@gmail.com>
2024-08-29 14:15:03 -0400

    Guardando cambios en triggers antes de cambiar de rama

--- routes/ventas/subfunciones/historial_ventas.py
+++ routes/ventas/subfunciones/historial_ventas.py
@@ -3,9 +3,13 @@ import sqlite3
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for, flash
 from ...database import get_db
 from urllib.parse import quote, unquote
+import os
+from openpyxl import load_workbook
 
 historial_ventas_bp = Blueprint('historial_ventas', __name__)
-    
+
+ruta_libro = '/home/apolito/Programacion/Proyectos/interfaz_web/DATABASE/NDE_PLANTILLA.xlsx'
+
 @historial_ventas_bp.route('/historial_ventas')
 def historial_ventas():
     db = get_db()
@@ -72,7 +76,6 @@ def historial_ventas():
 
 @historial_ventas_bp.route('/eliminar_venta/<int:numero_venta>', methods=['POST'])
 def eliminar_venta(numero_venta):
-    # print('hooooooooooooola')
     db = get_db()
     cursor = db.cursor()
     
@@ -81,38 +84,42 @@ def eliminar_venta(numero_venta):
     venta = cursor.fetchone()
 
     if venta:
-        cursor.execute('DELETE FROM Ventas WHERE numero_venta = ?', (numero_venta,))
-        db.commit()
-        flash('Venta eliminada correctamente', 'success')
-    else:
-        flash('La venta no existe', 'error')
-
-    # Seleccionar la factura en base al número de venta a eliminar
-    cursor.execute('SELECT monto_total, cliente_id FROM Facturas WHERE numero_venta = ?', (numero_venta,))
-    factura = cursor.fetchone()
-
-    if factura:
-        # Conseguir el valor del monto total de la factura y el cliente_id
-        monto_factura = factura[0]  # Nota: usando índices porque fetchone() devuelve una tupla
-        cliente_id = factura[1]
-
-        # Seleccionar el monto total de factura para hacer la resta con el monto de la factura
-        cursor.execute('SELECT monto_total FROM Facturas WHERE cliente_id = ?', (cliente_id,))
+        # Seleccionar la factura asociada a la venta
+        cursor.execute('SELECT monto_total, cliente_id FROM Facturas WHERE numero_venta = ?', (numero_venta,))
         factura = cursor.fetchone()
 
         if factura:
-            monto_total_deuda = factura[0]
-            monto_final_deuda = monto_total_deuda - monto_factura
+            monto_factura = factura[0]
+            cliente_id = factura[1]
 
-            # Actualizar la deuda en la tabla Deudas
-            cursor.execute('UPDATE Deudas SET monto_total = ? WHERE cliente_id = ?', (monto_final_deuda, cliente_id))
+            # Eliminar la venta
+            cursor.execute('DELETE FROM Ventas WHERE numero_venta = ?', (numero_venta,))
 
-        # Aquí agregarías el código para eliminar la factura
-        cursor.execute('DELETE FROM Facturas WHERE numero_venta = ?', (numero_venta,))
+            # Eliminar la factura
+            cursor.execute('DELETE FROM Facturas WHERE numero_venta = ?', (numero_venta,))
 
-    # Recuerda hacer commit después de realizar cambios
-    db.commit()
+            # Actualizar la deuda del cliente
+            cursor.execute('SELECT monto_total FROM Deudas WHERE cliente_id = ?', (cliente_id,))
+            deuda_actual = cursor.fetchone()
 
+            if deuda_actual:
+                monto_total_deuda = deuda_actual[0]
+                monto_final_deuda = monto_total_deuda - monto_factura
+
+                # Solo actualizar si la deuda es positiva
+                if monto_final_deuda >= 0:
+                    cursor.execute('UPDATE Deudas SET monto_total = ? WHERE cliente_id = ?', (monto_final_deuda, cliente_id))
+                else:
+                    cursor.execute('DELETE FROM Deudas WHERE cliente_id = ?', (cliente_id,))
+        else:
+            flash('Factura no encontrada para la venta', 'error')
+
+        db.commit()
+        flash('Venta y factura eliminadas correctamente', 'success')
+    else:
+        flash('La venta no existe', 'error')
+
+    db.close()
     return '', 200
 
 
@@ -153,3 +160,102 @@ def eliminar_producto(numero_venta, id):
         print('si llegaste hasta aqui, no tas fino mirei')
 
     return '', 200  
+
+
+@historial_ventas_bp.route('/generar_nota_entrega', methods=['GET', 'POST'])
+def generar_nota_entrega():
+    if request.method == 'POST':
+        numero_venta = request.form.get('numero_venta')
+        datos_cliente = obtener_datos_cliente_y_venta(numero_venta)
+        
+        if not datos_cliente:
+            return jsonify({'mensaje': 'Cliente o venta no encontrado'}), 404
+           
+        libro = abrir_libro_excel(ruta_libro)
+        ruta_archivo = crear_nota_entrega(libro, datos_cliente)
+        
+        # Aquí puedes redirigir a una página de éxito o simplemente renderizar una plantilla de confirmación
+        return render_template('crear_notas_entrega.html', ruta_archivo=ruta_archivo)
+    else:
+        return render_template('crear_notas_entrega.html')
+    
+def abrir_libro_excel(ruta_archivo):
+    return load_workbook(ruta_archivo)
+
+def obtener_datos_cliente_y_venta(numero_venta):
+    db = get_db()
+    cursor = db.cursor()
+
+    # Obtener el nombre del cliente basado en el número de venta
+    cursor.execute("SELECT cliente FROM Ventas WHERE numero_venta = ?", (numero_venta,))
+    cliente = cursor.fetchone()
+    if not cliente:
+        return None
+    
+    nombre_cliente = cliente[0]
+
+    # Obtener datos del cliente
+    cursor.execute("SELECT razon_social, rif_cedula, direccion, telefono FROM Clientes WHERE nombre_cliente = ?", (nombre_cliente,))
+    cliente = cursor.fetchone()
+    if not cliente:
+        return None
+    
+    razon_social, rif_cedula, direccion, telefono = cliente
+    
+    # Obtener ventas asociadas al número de venta específico
+    cursor.execute("SELECT numero_venta, producto, cantidad, precio, fecha FROM Ventas WHERE numero_venta = ?", (numero_venta,))
+    ventas = cursor.fetchall()
+    
+    db.close()
+    
+    return {
+        'nombre_cliente': nombre_cliente,
+        'razon_social': razon_social,
+        'rif_cedula': rif_cedula,
+        'direccion': direccion,
+        'telefono': telefono,
+        'ventas': [
+            {'numero_venta': numero_venta, 'producto': producto, 'cantidad': cantidad, 'precio': precio, 'fecha': fecha}
+            for numero_venta, producto, cantidad, precio, fecha in ventas
+        ]
+    }
+
+def crear_nota_entrega(libro, datos_cliente):
+    hoja = libro.active  # Supongamos que la plantilla usa la hoja activa
+    
+    # Insertar datos del cliente
+    hoja['I3'] = datos_cliente['razon_social']
+    hoja['AC3'] = datos_cliente['rif_cedula']
+    hoja['F5'] = datos_cliente['direccion']
+    hoja['AC7'] = datos_cliente['telefono']
+
+    # Obtener fecha y número de venta del primer elemento en las ventas
+    if datos_cliente['ventas']:
+        primer_venta = datos_cliente['ventas'][0]
+        hoja['G7'] = primer_venta['fecha']
+        hoja['AF1'] = primer_venta['numero_venta']
+
+    # Insertar datos de las ventas
+    fila_inicio = 12  # Supongamos que comenzamos a escribir las ventas en la fila 7
+    for venta in datos_cliente['ventas']:   
+            hoja[f'D{fila_inicio}'] = venta['producto']
+            hoja[f'A{fila_inicio}'] = venta['cantidad']
+            hoja[f'Y{fila_inicio}'] = venta['precio']
+            fila_inicio += 1
+    
+    # Definir la ruta de guardado
+    #ruta_guardar = "home/apolito/Programacion/Proyectos/interfaz_web/DATABASE/Notas_de_entrega"
+    ruta_guardar = "/mnt/c/Trabajo/Notas_de_entrega"
+    if not os.path.exists(ruta_guardar):
+        os.makedirs(ruta_guardar)
+    
+    # Crear el nombre del archivo
+    nombre_archivo = f"NDE_{primer_venta['numero_venta']}_{datos_cliente['nombre_cliente']}.xlsx"
+    
+    # Ruta completa
+    ruta_completa = os.path.join(ruta_guardar, nombre_archivo)
+    
+    # Guardar archivo
+    libro.save(ruta_completa)
+    
+    return ruta_completa
